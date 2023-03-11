65c4c81a17006420108c01397d88637d77d16c45
unknown <agustinentrocassi@gmail.com>
2023-03-10 23:16:37 -0300

    Subida de archivos con examen completo

---
+++ 03.js
@@ -0,0 +1,32 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function fakeBinary2(string) {
+  // La funcion llamada 'fakeBinary' recibe como argumento un string con numeros
+  // y debe devolver un string en donde se remplaza los numeros menores de 5 con '0'
+  // y los numeros iguales o mayores a 5 con '1'
+  // Por ej:
+  // fakeBinary2('5627') devuelve "1101"
+  // fakeBinary("729") devuelve "101"
+  // Tu código aca:
+  let arrayBinario = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] >= 5) {
+      arrayBinario.push(1)
+    }
+    else {
+      arrayBinario.push(0)
+    }
+  }
+  
+  let stringBinario = arrayBinario.join("")
+  return stringBinario
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = fakeBinary2
